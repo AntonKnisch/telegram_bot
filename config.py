49d7d539a69dd848a906029cc9f26032c46fe651
Anton Knisch <110488280+AntonKnisch@users.noreply.github.com>
2022-10-26 03:01:55 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+TOKEN = "5608749341:AAFNhEEBZunKZUevrCdlRBJW21Ma8FliRUo"
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'тенге': 'KZT',
+    'юань': 'CNY'
+}
